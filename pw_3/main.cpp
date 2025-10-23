0b8a8de2b3b42c096bcc2db51eb758b4a4a6d74b
Cyrill <kirillp001100@gmail.com>
2025-10-24 02:27:45 +0300

    3pw

---
+++ pw_3/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    
+    int option,
+    capacity = 4,
+    taskCount = 0,
+    Id;
+    char choice;
+    int *tasksList = new int[capacity];
+
+    do {
+        do {
+            cout << "Выберите опцию:" << endl
+            << "1. Добавить новую задачу;" << endl
+            << "2. Удалить задачу;" << endl 
+            << "3. Просмотреть все задачи;" << endl
+            << "4. Выйти." << endl << "> "; 
+            cin >> option;
+        } while (!(1 <= option && option <= 5));
+
+        switch (option) {
+            case 1:
+                if(taskCount == capacity) {
+                    capacity *= 2;
+                    int* newTasks = new int[capacity];
+                    for (int i = 0; i < taskCount; i++) {
+                        newTasks[i] = tasksList[i];
+                    }
+                    delete[] tasksList;
+                    tasksList = newTasks;
+                }
+
+                cout << "Введите id задачи: ";
+                cin >> Id;
+
+                tasksList[taskCount] = Id;
+                taskCount++;
+                cout << "Задача номер " << Id << " успешно добавлена" << endl;
+            case 2:
+                if (taskCount == 0) {
+                    cout << "Список задач пуст" << endl;
+                    continue;
+                } else {
+                    cout << "Текущие задачи: ";
+                    for (int i = 0; i < taskCount; i++) {
+                        cout << tasksList[i] << ", ";
+                    }
+                    cout << endl;
+                    int deleteId;
+                    cout << "Введите номер задачи, которую нужно удалить: ";
+                    cin >> deleteId;
+
+                    bool found = false;
+                    for (int i = 0; i < taskCount; i++) {
+                        if (tasksList[i] == deleteId) {
+                            found = true;
+                            for (int j = i; j < taskCount - 1; j++) {
+                                tasksList[j] = tasksList[j + 1];
+                            }
+                        }
+                    }
+                }
+
+
+        }
+
+
+
+
+        cout << endl << "Хотите продолжить? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y');
+    cout << "Пока-пока!";
+}
