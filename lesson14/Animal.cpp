e60442c1efc6e0d80f6436073145127e831096f6
Cyrill <kirillp001100@gmail.com>
2025-11-10 03:08:35 +0300

    Штуки дрюки

--- lesson14/Animal.cpp
+++ lesson14/Animal.cpp
@@ -11,10 +11,51 @@ Animal::Animal(string name, string species, string color, int age, float weight)
     this->age = age;
     this->weight = weight;
 }
+Animal::Animal(string name, string color, int age, float weight) {
+    this->name = name;
+    this->species = "обыкновенный";
+    this->color = color;
+    this->age = age;
+    this->weight = weight;
+}
 
 Animal::~Animal() {
     cout << name << " уничтожен!";
 }
+
+// геттеры
+const string Animal::getName() { return name; }
+const string Animal::getSpecies() { return species; }
+const string Animal::getColor() { return color; }
+const int Animal::getAge() { return age }
+const float Animal::getWeight() { return weight; }
+
+// сеттеры
+void Animal::setName(const string name) {
+    this->name = name;
+}
+
+void Animal::setSpecies(const string species) {
+    this->species = species;
+}
+
+void Animal::setColor(const string color) {
+    this->color = color;
+}
+
+void Animal::setAge(const int age) {
+    this->age = age;
+}
+
+void Animal::setWeight(const float weight) {
+    this->weight = weight;
+}
+void setSpecies(const string species);
+void setColor(const string color);
+void setAge(const int age);
+void setWeight(const float weight);
+
+// остальные методы
 void Animal::increaseWheight(int days_of_kormezhka) {
     float additional_weight = weight * 0.01 * days_of_kormezhka;
     weight += additional_weight;
--- lesson14/Animal.h
+++ lesson14/Animal.h
@@ -13,8 +13,24 @@ class Animal {
     
     public:
         Animal(string name, string species, string color, int age, float weight);
-        Animal(string name, string species, string color, int age, float weight);
+        Animal(string name, string color, int age, float weight);
         ~Animal();
+
+        // геттеры
+        const string getName();
+        const string getSpecies();
+        const string getColor();
+        const int getAge();
+        const float getWeight();
+
+        // сеттеры
+        void setName(const string name);
+        void setSpecies(const string species);
+        void setColor(const string color);
+        void setAge(const int age);
+        void setWeight(const float weight);
+
+        // остальные методы
         void increaseWheight(int days_of_kormezhka);
         void changeName(string new_name);
         void printInfo();
